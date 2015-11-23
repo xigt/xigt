fbbb0fc0e27639a2ddc4d37c95ece069bceac166
Michael Wayne Goodman <goodman.m.w@gmail.com>
2015-11-23 12:49:08 -0800

    Toolbox importer creates phrase tier from words.

--- xigt/importers/toolbox.py
+++ xigt/importers/toolbox.py
@@ -4,24 +4,35 @@
 #
 # default config.json:
 # {
-#   "tier_types": {
-#     "\\t": ["words", "w"],
-#     "\\m": ["morphemes", "m"],
-#     "\\g": ["glosses", "g"],
-#     "\\p": ["pos", "pos"],
-#     "\\f": ["translations", "t"],
-#   },
-#   "alignments": {
-#     "\\m": "\\t",
-#     "\\g": "\\m",
-#     "\\p": "\\m"
-#   },
 #   "record_markers": [
 #     "\\id",
 #     "\\ref"
 #   ],
-#   "attribute_map": {
+#   "igt_attribute_map": {
 #     "\\id": "corpus-id"
+#   },
+#   "tier_map": {
+#     "\\t": "w",
+#     "\\m": "m",
+#     "\\g": "g",
+#     "\\p": "pos",
+#     "\\f": "t"
+#   },
+#   "make_phrase_tier": ["w", "p"],
+#   "tier_types": {
+#     "p": {"type": "phrases"},
+#     "w": {"type": "words", "interlinear": true},
+#     "m": {"type": "morphemes", "interlinear": true},
+#     "g": {"type": "glosses", "interlinear": true},
+#     "pos": {"type": "pos", "interlinear": true},
+#     "t": {"type": "translations"}
+#   },
+#   "alignments": {
+#     "w": ["segmentation", "p"],
+#     "m": ["segmentation", "w"],
+#     "g": ["alignment", "m"],
+#     "pos": ["alignment", "m"],
+#     "t": ["alignment", "p"]
 #   },
 #   "error_recovery_method": "ratio"
 # }
@@ -46,27 +57,42 @@ except ImportError:
         '  https://github.com/goodmami/toolbox'
     )
 
-default_tier_types = {
-    '\\t': ['words', 'w'],
-    '\\m': ['morphemes', 'm'],
-    '\\g': ['glosses', 'g'],
-    '\\p': ['pos', 'pos'],
-    '\\f': ['translations', 't'],
-}
-
-default_alignments = {
-    '\\m': '\\t',
-    '\\g': '\\m',
-    '\\p': '\\m'
-}
-
 default_record_markers = [
     '\\id',
     '\\ref'
 ]
 
-default_attribute_map = {
+default_igt_attribute_map = {
     '\\id': 'corpus-id'
+}
+
+default_tier_map = {
+    "\\t": "w",
+    "\\m": "m",
+    "\\g": "g",
+    "\\p": "pos",
+    "\\f": "t"
+}
+
+# if set, join tokens from first id to make phrase tier with second id
+default_make_phrase_tier = ["w", "p"]
+
+default_tier_types = {
+    "p": {"type": "phrases"},
+    "w": {"type": "words", "interlinear": True},
+    "m": {"type": "morphemes", "interlinear": True},
+    "g": {"type": "glosses", "interlinear": True},
+    "pos": {"type": "pos", "interlinear": True},
+    "t": {"type": "translations"}
+}
+
+# align Xigt tier IDs, not Toolbox markers
+default_alignments = {
+    "w": ["segmentation", "p"],
+    "m": ["segmentation", "w"],
+    "g": ["alignment", "m"],
+    "pos": ["alignment", "m"],
+    "t": ["alignment", "p"]
 }
 
 default_error_recovery_method = 'ratio'
@@ -76,11 +102,16 @@ def xigt_import(infile, outfile, options=None):
 
     if options is None:
         options = {}
+    options.setdefault('record_markers', default_record_markers)
+    options.setdefault('igt_attribute_map', default_igt_attribute_map)
+    options.setdefault('tier_map', default_tier_map)
+    options.setdefault('make_phrase_tier', default_make_phrase_tier)
     options.setdefault('tier_types', default_tier_types)
     options.setdefault('alignments', default_alignments)
-    options.setdefault('record_markers', default_record_markers)
-    options.setdefault('attribute_map', default_attribute_map)
     options.setdefault('error_recovery_method', default_error_recovery_method)
+
+    # just use existing info to create marker-based alignment info
+    options['tb_alignments'] = _make_tb_alignments(options)
 
     with open(infile, 'r') as in_fh, open(outfile, 'w') as out_fh:
         tb = toolbox.read_toolbox_file(in_fh)
@@ -89,7 +120,20 @@ def xigt_import(infile, outfile, options=None):
         xigtxml.dump(out_fh, xc)
 
 
+def _make_tb_alignments(opts):
+    inv_tier_map = {t: m for m, t in opts['tier_map'].items()}
+    interlinear = {t: d.get('interlinear', False)
+                   for t, d in opts['tier_types'].items()}
+    tb_alignments = {}
+    for t1, aln in opts['alignments'].items():
+        refattr, t2 = aln
+        if interlinear.get(t1, False) and interlinear.get(t2, False):
+            tb_alignments[inv_tier_map[t1]] = inv_tier_map[t2]
+    return tb_alignments
+
+
 def toolbox_igts(tb, options):
+    corpus_opts = options.get('xigt-corpus', {})
     record_markers = options['record_markers']
     assert len(record_markers) > 0
     mkrPriKey = record_markers[-1]
@@ -111,7 +155,7 @@ def make_igt(key, data, context, options):
     if context is None:
         context = {}
     attrs = {}
-    attmap = options['attribute_map']
+    attmap = options['igt_attribute_map']
     for (mkr, val) in chain(data, context.items()):
         if val is None:  # will be None if mkr not encountered
             continue
@@ -148,50 +192,72 @@ def make_igt(key, data, context, options):
 
 
 def make_all_tiers(item_data, options):
+    tier_map = options['tier_map']
     tier_types = options['tier_types']
     alignments = options['alignments']
-    aligned_tiers = set(alignments.keys()).union(alignments.values())
+    tb_alignments = options['tb_alignments']
+    phrase_opts = options.get('make_phrase_tier')
+    aligned_tiers = set(tb_alignments.keys()).union(tb_alignments.values())
     # use strip=False because we want same-length strings
     tier_data = toolbox.normalize_record(item_data, aligned_tiers, strip=False)
     prev = {}
     aligned_fields = toolbox.align_fields(
-        tier_data, alignments, errors=options['error_recovery_method']
+        tier_data, tb_alignments, errors=options['error_recovery_method']
     )
-    for mkr, aligned_tokens in aligned_fields:
-        if mkr not in tier_types:
+    for mkr, aln_tokens in aligned_fields:
+        if mkr not in tier_map:
             continue
-        tier_type, tier_id = tier_types.get(mkr)
-        algn_tier = prev.get(alignments.get(mkr))  # could be None
+        tier_id = tier_map.get(mkr)
+        if phrase_opts and phrase_opts[0] == tier_id:
+            tier = make_phrase_tier(phrase_opts[1], aln_tokens)
+            prev[phrase_opts[1]] = tier
+            yield tier
+        tier_type = tier_types[tier_id].get('type')
+        refattr, aln_id = alignments.get(tier_id, (None, None))
+        algn_tier = prev.get(aln_id)  # could be None
         try:
-            tier = make_tier(tier_type, tier_id, aligned_tokens, algn_tier)
+            tier = make_tier(tier_type, tier_id,
+                             refattr, aln_tokens, algn_tier)
         except (AttributeError, AssertionError):
             raise XigtImportError('Error making {} tier (marker: {}).'
                                   .format(tier_type, mkr))
-        prev[mkr] = tier
+        prev[tier_id] = tier
         yield tier
 
 
-def make_tier(tier_type, tier_id, aligned_tokens, algn_tier):
+def make_phrase_tier(tier_id, aln_tokens):
+    return Tier(
+        id=tier_id,
+        type='phrases',
+        items=[
+            Item(
+                id='{}1'.format(tier_id),
+                text=' '.join(t for aln in aln_tokens for t in aln[1])
+            )
+        ]
+    )
+
+
+def make_tier(tier_type, tier_id, refattr, aln_tokens, algn_tier):
     attrs = OrderedDict()
     items = list()
     i = 1  # start indices at 1
-    if aligned_tokens == [(None, None)]:
+    if aln_tokens == [(None, None)]:
         pass  # nothing to do
-    elif algn_tier is not None:
-        attrs['alignment'] = algn_tier.id
-        algn_data = zip_longest(algn_tier.items, aligned_tokens)
+    elif refattr is not None and algn_tier is not None:
+        attrs[refattr] = algn_tier.id
+        algn_data = zip_longest(algn_tier.items, aln_tokens)
         for tgt_item, src_data in algn_data:
             tgt_tok, src_toks = src_data
-            assert tgt_tok == tgt_item.text  # FIXME is this necessary?
             for s in src_toks:
                 items.append(
                     Item(id='{}{}'.format(tier_id, i),
                          text=s,
-                         attributes={'alignment':tgt_item.id})
+                         attributes={refattr:tgt_item.id})
                 )
                 i += 1
     else:
-        for tgt, src in aligned_tokens:
+        for tgt, src in aln_tokens:
             for s in src:
                 items.append(Item(id='{}{}'.format(tier_id, i), text=s))
                 i += 1
